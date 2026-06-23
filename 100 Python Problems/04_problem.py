b7998a8675ee43db94877cf7bc82da697ee9c1b2
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-06-23 19:38:40 +0530

    Add files via upload

---
+++ 100 Python Problems/04_problem.py
@@ -0,0 +1,14 @@
+# Task
+# The provided code stub reads an integer,n, from STDIN.
+# For all non-negative integers i<n, print i**2.
+
+# Example
+# n = 3
+# The list of non-negative integers that are less than n=3 is [0,1,2]. 
+# Print the square of each number on a separate line.
+
+n = int(input())
+for i in range(0, n):
+    e = i**2
+    print(e)
+    
