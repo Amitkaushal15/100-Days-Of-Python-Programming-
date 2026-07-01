e8f23844fd84f49981b3df54c69c3ef0f7520dd2
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-01 21:16:39 +0530

    Add files via upload

---
+++ 100 Python Problems/Variables/09_variable.py
@@ -0,0 +1,7 @@
+# 9.	Take three numbers and print their average. 
+
+a = int(input("Enter the 1st number: "))
+b = int(input("Enter the 2nd number: "))
+c = int(input("Enter the 3rd number: "))
+average = (a+b+c)/3
+print(f"average of {a}, {b}, {c} is {average}")
---
+++ 100 Python Problems/Variables/10_variable.py
@@ -0,0 +1,8 @@
+# 10.	Print a formatted introduction using f-strings.
+
+name = "Amit Kaushal"
+role = "Cybersecurity Engineer"
+experience = 4.5
+print(f"Hello! My name is {name}. I work as a {role}. i have {experience} years of experience.")
+
+
