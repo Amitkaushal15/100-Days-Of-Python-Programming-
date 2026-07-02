c63fa761802c06f1c16766619e906df4a2c4b926
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-02 17:11:41 +0530

    Add files via upload

---
+++ 100 Python Problems/Data Types/01_DataType.py
@@ -0,0 +1,23 @@
+# 1. Check the data type of a variable. 
+
+a = "Amit Kaushal"
+print(a, type(a))
+
+b = 68
+print(b, type(b))
+
+c = 9.8
+print(c, type(c))
+
+d = True
+print(d, type(d))
+
+e = [8, "Amit", False, 9.5]
+e.append(34)
+print(e, type(e))
+
+f = (2, "abcd", True)
+print(f, type(f))
+
+g = {1 : "Patna", 2 : "Darbhanga", 3 : "Bihar"}
+print(g, type(g))
---
+++ 100 Python Problems/Data Types/02_DataType.py
@@ -0,0 +1,9 @@
+# 2.	Convert integer to float. 
+
+x = 9
+a = float(x)
+print(type(x),type(a))
+
+b = True
+c = int(b)
+print(type(b), type(c))
