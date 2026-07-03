8fe12ccddec7eaf2c242032a00fd8624f5e0b3ae
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-03 18:33:16 +0530

    Add files via upload

---
+++ 100 Python Problems/Data Types/07_DataType.py
@@ -0,0 +1,11 @@
+# 7. Check whether a variable is boolean. 
+
+a = True
+print(type(a))
+
+
+x = True
+y = 1
+
+print(isinstance(x, bool))  # Returns True
+print(isinstance(y, bool))  # Returns False
