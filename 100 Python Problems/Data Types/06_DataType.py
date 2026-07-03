2467e8a13fbf41497ca8238431976eda52d11455
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-03 18:32:44 +0530

    Add files via upload

---
+++ 100 Python Problems/Data Types/06_DataType.py
@@ -0,0 +1,27 @@
+# 6. Create variables of all basic data types. 
+
+a = 45
+print(a, type(a))
+
+b = 9.7
+print(b, type(b))
+
+c = "Amit Kaushal"
+print(c, type(c))
+
+d = True
+print(d, type(d))
+
+e = 3 + 4j
+print(e, type(e))
+
+f = ["Amit", 45, False, 4.5]
+print(f, type(f))
+
+g = (36, 9.8, "Amit", True)
+print(g, type(g))
+
+h = {'Name: ' "Amit Kaushal", 'Age: ' "24", 'College: ' "IIT Patna"}
+print(h, type(h))
+
+
