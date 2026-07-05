a4a2b1458e80599f47bd57a0dc2d936768f21442
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-05 23:27:02 +0530

    Add files via upload

---
+++ 100 Python Problems/Operators/01_Operators.py
@@ -0,0 +1,10 @@
+# 1.	Perform addition, subtraction, multiplication, and division. 
+
+# Addition 
+
+a = 10
+b = 20
+print(f"Addition of {a} and {b} is {a+b}")
+print(f"Subtraction of {a} and {b} is {a-b}")
+print(f"Multiplication of {a} and {b} is {a*b}")
+print(f"Division of {a} and {b} is {a/b}")
