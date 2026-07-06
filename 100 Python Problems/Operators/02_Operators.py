e45afc405c8f9f36b30969d4ce7b1257da54dddb
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-06 15:25:40 +0530

    Add files via upload

---
+++ 100 Python Problems/Operators/02_Operators.py
@@ -0,0 +1,5 @@
+# 2. Find the remainder using %. 
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print(f"The remainder of {a} and {b} is {a%b}")
---
+++ 100 Python Problems/Operators/03_Operators.py
@@ -0,0 +1,5 @@
+# 3.	Find power using **. 
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+print(f"The Exponent of {a} and {b} is {a**b}")
