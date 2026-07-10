7ece0772eba8765b0ad23440b0f9e666dafeed0d
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-10 20:07:19 +0530

    Add files via upload

---
+++ 100 Python Problems/Operators/06_Operators.py
@@ -0,0 +1,8 @@
+# 6.	Check if two numbers are unequal. 
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if a != b:
+    print(f"it's unequal.")
+else:
+    print(f"It's equal.")
