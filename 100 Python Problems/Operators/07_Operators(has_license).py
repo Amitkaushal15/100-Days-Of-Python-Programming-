90e875e0ffa4019d31e6551cca967332ee6469c9
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-07-26 22:25:55 +0530

    Add files via upload

---
+++ 100 Python Problems/Operators/07_Operators(has_license).py
@@ -0,0 +1,11 @@
+# 7. Use logical AND.
+
+age = 25
+has_license = True
+
+# Both conditions must evaluate to True
+if age >= 18 and has_license:
+    print("You are legally allowed to drive.")
+else:
+    print("You cannot drive.")
+    
