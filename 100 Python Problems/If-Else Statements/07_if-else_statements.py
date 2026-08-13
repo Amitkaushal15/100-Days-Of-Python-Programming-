647b32113f16346657e01490735f631c3c385392
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-08-13 23:56:42 +0530

    Add files via upload

---
+++ 100 Python Problems/If-Else Statements/07_if-else_statements.py
@@ -0,0 +1,8 @@
+# 07. Check whether a character is a vowel
+
+character = input("Enter a character: ")
+
+if character == 'a' or character == 'e' or character == 'i' or character == 'o' or character == 'u':
+    print("The character is a vowel")
+else:
+    print("The character is not a vowel")
