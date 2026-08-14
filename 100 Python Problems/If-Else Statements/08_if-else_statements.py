38b874d5d76bb8ef69133abf1c22fb0f34ef7684
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-08-14 21:33:39 +0530

    Add files via upload

---
+++ 100 Python Problems/If-Else Statements/08_if-else_statements.py
@@ -0,0 +1,8 @@
+# 8.	Check if a number is divisible by 5. 
+
+a = int(input("Enter the number: "))
+
+if a % 5 == 0:
+    print(f"{a} is divisible by 5.")
+else:
+    print(f"{a} is not divisible by 5.")
