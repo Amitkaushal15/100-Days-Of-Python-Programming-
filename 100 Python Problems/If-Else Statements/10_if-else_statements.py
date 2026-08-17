aa27b10671aa65f5546f1d90588d8020e11fb3bf
Amit Kaushal <61357307+Amitkaushal15@users.noreply.github.com>
2026-08-17 13:20:07 +0530

    Add files via upload

---
+++ 100 Python Problems/If-Else Statements/10_if-else_statements.py
@@ -0,0 +1,8 @@
+# 10.	Check if a year is a century year.
+
+year = int(input("Enter the year: "))
+
+if year % 100 == 0:
+    print(f"{year} is a century year.")
+else:
+    print(f"{year} is not a century year.")
